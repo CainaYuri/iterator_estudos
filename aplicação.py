6577755339f730cb8ec50a713a4eec9ea190996b
CainaYuri <cainayuri@gmail.com>
2025-06-03 12:42:05 -0300

    Commit

---
+++ aplicação.py
@@ -0,0 +1,35 @@
+class vendasIterator:
+    def __init__(self, arquivo):
+        self.arquivo = arquivo
+        self.file = open(arquivo, 'r', encoding='utf-8')
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        linha = self.file.readline()
+        if not linha:
+            self.file.close()
+            raise StopIteration
+        categoria, valor = linha.strip().split(',')
+        return categoria, float(valor)
+
+def gerador_somA_vendas(arquivo):
+    total_vendas =0
+    for categoria, valor in vendasIterator(arquivo):
+        total_vendas += valor
+        yield categoria, valor, total_vendas
+
+contagem_vendas ={}
+arquivo = "vendas.txt"
+print("Processando Vendas....")
+for categoria, valor, total in gerador_somA_vendas(arquivo):
+    print(f"Categoria; {categoria}, Valor R$,{valor:.2f}, Total R$: {total:.2f}")
+
+    if categoria not in contagem_vendas:
+        contagem_vendas[categoria] = 0
+    contagem_vendas[categoria] +=1
+
+for categoria, contagem in contagem_vendas.items():
+    print(f"{categoria}: {contagem}: vendas")
+
